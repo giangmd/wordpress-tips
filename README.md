86385d28814cc3c665ed0715200c2222f16c5571
Giang Peter <giangmd93@gmail.com>
2018-09-24 14:26:24 +0700

    create template email

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Some code about wordpress can help your code become faster
 ## Getting Started
 * [Function in functions.php file](./functions.php)
 * [Query mysql](./mysql-query.sql)
-* [Query mysql](./mysql-query.sql)
 * [Htaccess config](./.htaccess)
--- functions.php
+++ functions.php
@@ -7,3 +7,7 @@ function wp_nav_parent_class( $classes, $item ) {
     return $classes;
 }
 add_filter('nav_menu_css_class', 'wp_nav_parent_class', 10, 2);
+
+
+// create product woocomerce by wp_insert_post
+update_post_meta( $new_product_post_id, '_visibility', 'visible' );
